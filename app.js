cd79304b719695772b488d79ad677894973a678a
John Marcos <johnsaguay@gmail.com>
2021-11-02 19:10:50 -0700

    Users can create events

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const models = require('./db/models')
 const express = require('express');
 const app = express();
 
@@ -5,6 +6,9 @@ const app = express();
 const exphbs = require('express-handlebars');
 const Handlebars = require('handlebars');
 const {allowInsecurePrototypeAccess} = require('@handlebars/allow-prototype-access')
+// Initialize body-parser
+const bodyParser = require('body-parser')
+app.use(bodyParser.urlencoded({ extended: true}));
 //use main as our default layout
 app.engine('handlebars', exphbs({ defaultLayout: 'main', handlebars: allowInsecurePrototypeAccess(Handlebars)}));
 // use handlebars to render
@@ -15,11 +19,26 @@ var events = [
     { title: "I am your first event", desc: "A great event that is super fun to look at and good", imgUrl: "https://img.purch.com/w/660/aHR0cDovL3d3dy5saXZlc2NpZW5jZS5jb20vaW1hZ2VzL2kvMDAwLzA4OC85MTEvb3JpZ2luYWwvZ29sZGVuLXJldHJpZXZlci1wdXBweS5qcGVn" },
     { title: "I am your second event", desc: "A great event that is super fun to look at and good", imgUrl: "https://img.purch.com/w/660/aHR0cDovL3d3dy5saXZlc2NpZW5jZS5jb20vaW1hZ2VzL2kvMDAwLzA4OC85MTEvb3JpZ2luYWwvZ29sZGVuLXJldHJpZXZlci1wdXBweS5qcGVn" },
     { title: "I am your third event", desc: "A great event that is super fun to look at and good", imgUrl: "https://img.purch.com/w/660/aHR0cDovL3d3dy5saXZlc2NpZW5jZS5jb20vaW1hZ2VzL2kvMDAwLzA4OC85MTEvb3JpZ2luYWwvZ29sZGVuLXJldHJpZXZlci1wdXBweS5qcGVn" }
-  ]
-  // INDEX
+];
+// GETS ALL EVENTS AND DISPLAYS THEM
 app.get('/', (req, res) => {
-    res.render('events-index', { events: events });
+    models.Event.findAll({ order: [['createdAt', 'DESC']] }).then(events => {
+      res.render('events-index', { events: events});
+    })
   })
+
+app.get('/events/new', (req, res) => {
+  res.render('events-new', {})
+})
+//Create
+app.post('/events', (req, res) => {
+  models.Event.create(req.body).then(event => {
+    res.redirect('/');
+  }).catch((err) => {
+    console.log(err)
+  });
+})
+
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
