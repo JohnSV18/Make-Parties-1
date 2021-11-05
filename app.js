04545a612cb81bcd6df3c56d5ce063b25f0647ce
John Marcos <johnsaguay@gmail.com>
2021-11-05 12:15:51 -0700

    Users can destroy events

--- app.js
+++ app.js
@@ -63,6 +63,15 @@ app.put('/events/:id', (req, res) => {
     console.log(err);
   });
 });
+// DELETE
+app.delete('/events/:id', (req, res) => {
+  models.Event.findByPk(req.params.id).then(event => {
+    event.destroy();
+    res.redirect('/')
+  }).catch((err) => {
+    console.log(err);
+  });
+})
 
 const port = process.env.PORT || 3000;
 
