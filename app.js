dee1ebf9278ebc7bb3954063a104eb79f180eb52
John Marcos <johnsaguay@gmail.com>
2021-11-05 11:51:54 -0700

    Users can edit and update events

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const models = require('./db/models')
+const methodOverride = require('method-override')
 const express = require('express');
 const app = express();
 
@@ -9,6 +10,7 @@ const {allowInsecurePrototypeAccess} = require('@handlebars/allow-prototype-acce
 // Initialize body-parser
 const bodyParser = require('body-parser')
 app.use(bodyParser.urlencoded({ extended: true}));
+app.use(methodOverride('_method'))
 //use main as our default layout
 app.engine('handlebars', exphbs({ defaultLayout: 'main', handlebars: allowInsecurePrototypeAccess(Handlebars)}));
 // use handlebars to render
@@ -31,7 +33,7 @@ app.post('/events', (req, res) => {
   }).catch((err) => {
     console.log(err)
   });
-})
+});
 // SHOW
 app.get('/events/:id', (req, res) => {
   // find an event by it's id that was passed through req.params
@@ -41,9 +43,29 @@ app.get('/events/:id', (req, res) => {
     console.log(err.message)
   })
 });
+// EDIT
+app.get('/events/:id/edit', (req, res) => {
+  models.Event.findByPk(req.params.id).then((event) => {
+    res.render('events-edit', { event:event });
+  }).catch((err) => {
+    console.log(err.message)
+  })
+});
+// UPDATE
+app.put('/events/:id', (req, res) => {
+  models.Event.findByPk(req.params.id).then(event => {
+    event.update(req.body).then(event => {
+      res.redirect(`/events/${req.params.id}`);
+    }).catch((err) => {
+      console.log(err)
+    });
+  }).catch((err) => {
+    console.log(err);
+  });
+});
 
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
     console.log('Server listening on port 3000!')
-})
+});
